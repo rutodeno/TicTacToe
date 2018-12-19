c966a6203a3dbc701340645e182d46e28cc84077
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-12-18 21:44:34 -0500

    added user2 input. Still have to figure issue with scoping withing function replaceValue

--- TicTacToe_Project/main.c
+++ TicTacToe_Project/main.c
@@ -37,26 +37,25 @@ int main()
 	printf("This is a TicTacToe Game. To start, using the keypad, select the number to point to where you want to play\n");
 	printf("For example, entering 9 will place X on box 9 of the matrix.\n");
 	printf("\n");
+	printTicTacToe(x, y, tictac_array); // print array
+
 
 	while(!gameplay) // while the game is not over
 	{
-		
-		//printTicTacToe(x, y, tictac_array);
-
 		printf("User 1, play enter your selection: \n");
 		scanf_s("%d", &user_1);
 		printf("User 1 entered %d \n", user_1);
-		int newarray = replaceValue(user_1, tictac_array);
-		printTicTacToe(x, y, newarray);
+		replaceValue("user1", user_1, tictac_array);
+		printTicTacToe(x, y, tictac_array);
 
-
-
-		/*
+		
 		printf("User 2, please enter your selection: \n");
 		scanf_s("%d", &user_2);
 		printf("User 2 entered %d \n", user_2);
-		*/
-		gameplay = true;
+		replaceValue("user2", user_2, tictac_array);
+		printTicTacToe(x, y, tictac_array);
+
+		//gameplay = true;
 	}
 	
 	system("pause");
@@ -69,42 +68,55 @@ void printTicTacToe(int x, int y, int array[3][3])
 	{
 		for (x = 0; x < 3; x++)
 		{
-			printf("[%c]", array[y][x]);
+			printf("[%d]", array[y][x]);
 		}
 		printf("\n");
 	}
 
 }
-int replaceValue(int userValue, int array[3][3])
+
+
+int replaceValue(char user[], int userValue, int array[3][3])
 {
+	int initial;
+
+	if (user == "user1")
+	{
+		initial = 0;
+	}
+	else if (user == "user2")
+	{
+		initial = 1;
+	}
+	
 	switch (userValue)
 	{
 		case 1 :
-			array[0][0] =  char X;
+			array[0][0] =   initial;
 			break;
 		case 2:
-			array[0][1] = 'X';
+			array[0][1] = initial;
 			break;
 		case 3:
-			array[0][2] = 'X';
+			array[0][2] = initial;
 			break;
 		case 4:
-			array[1][0] = 'X';
+			array[1][0] = initial;
 			break;
 		case 5:
-			array[1][1] = 'X';
+			array[1][1] = initial;
 			break;
 		case 6:
-			array[1][2] = 'X';
+			array[1][2] = initial;
 			break;
 		case 7:
-			array[2][0] = 'X';
+			array[2][0] = initial;
 			break;
 		case 8:
-			array[2][1] = 'X';
+			array[2][1] = initial;
 			break;
 		case 9:
-			array[2][2] = 'X';
+			array[2][2] = initial;
 			break;
 		default:
 			printf("Please enter a valid input");
