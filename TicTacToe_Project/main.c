48ec1ab15f4ce99adde6502d7e084240cbf7654c
Dennis Ruto <34184017+rutodeno@users.noreply.github.com>
2018-12-21 15:57:49 -0500

    simplified code to enhance readability. Still have an 'Access location error'

--- TicTacToe_Project/main.c
+++ TicTacToe_Project/main.c
@@ -9,114 +9,112 @@ Once this is accomplished, we can move into enabling two(2) users.
 #include <stdio.h>
 #include <stdbool.h>
 
-void printTicTacToe(int x, int y, int array[3][3]);
-int replaceValue(int userValue, int array[3][3]);
+char inputSelection(int userNumber);
+char replaceValue(char user, char userValue, char array[3][3]);
 
 
 int main()
 {
 	int x = 0;
 	int y = 0;
-	int counter = 1;
-	int user_1, user_2;
+	char userInput1;
+	char userInput2;
+	int counter = 0;
+	bool user_1 = false;
+	bool user_2 = false;
 	bool gameplay = false;
 	
-	int tictac_array[3][3];
+	char tictac_array[3][3] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
 
-	for (y = 0; y < 3; y++)
-	{
-		for (x = 0; x < 3; x++)
-		{
-			tictac_array[y][x] = counter++;
-		}
-	}
-
-	printf("////////////////////////////////////////////////////////////////////////////\n");
-	printf("////////////////////////////////////////////////////////////////////////////\n");
-	printf("////////////////////////////////////////////////////////////////////////////\n");
-	printf("This is a TicTacToe Game. To start, using the keypad, select the number to point to where you want to play\n");
-	printf("For example, entering 9 will place X on box 9 of the matrix.\n");
+	// printing out the double array
+	printf("User 1 (X)   User 2 (O) \n");
 	printf("\n");
-	printTicTacToe(x, y, tictac_array); // print array
 
-
-	while(!gameplay) // while the game is not over
-	{
-		printf("User 1, play enter your selection: \n");
-		scanf_s("%d", &user_1);
-		printf("User 1 entered %d \n", user_1);
-		replaceValue("user1", user_1, tictac_array);
-		printTicTacToe(x, y, tictac_array);
-
-		
-		printf("User 2, please enter your selection: \n");
-		scanf_s("%d", &user_2);
-		printf("User 2 entered %d \n", user_2);
-		replaceValue("user2", user_2, tictac_array);
-		printTicTacToe(x, y, tictac_array);
-
-		//gameplay = true;
-	}
-	
-	system("pause");
-	return 0;
-}
-
-void printTicTacToe(int x, int y, int array[3][3])
-{
 	for (y = 0; y < 3; y++)
 	{
 		for (x = 0; x < 3; x++)
 		{
-			printf("[%d]", array[y][x]);
+			printf("[%c]", tictac_array[y][x]);
 		}
 		printf("\n");
 	}
 
+	printf("\n");
+
+	while (counter < 9) 
+	{
+		user_1 = true;
+		if (user_1 && !user_2) 
+		{
+			char user = 'X';
+			userInput1 = inputSelection(1);
+			//printf("User 1 selected: %c\n", userInput1);
+			replaceValue(user, userInput1, tictac_array[3][3]);
+			user_2 = true;
+			user_1 = false;
+
+			
+		} else
+		{
+			char user = 'O';
+			userInput2 = inputSelection(2);
+			//printf("User 2 selected: %c\n", userInput2);
+			replaceValue(user, userInput2, tictac_array[3][3]);
+			user_1 = true;
+			user_2 = false;
+		}
+		counter++;
+	}
+
+	printf("DRAW !! Game Over! \n");
+	system("pause");
+	return 0;
+}
+
+char inputSelection(int userNumber)
+{	
+	char line[10];
+	char userInput;
+	printf("\rUser %d please enter your selection: ", userNumber);
+	fflush( stdout );
+	Sleep(1);
+	fgets(line,sizeof(line), stdin);
+	sscanf_s(line, " %c", &userInput);
+	return  userInput;
 }
 
 
-int replaceValue(char user[], int userValue, int array[3][3])
+char replaceValue(char user, char userValue, char array[3][3])
 {
-	int initial;
-
-	if (user == "user1")
-	{
-		initial = 0;
-	}
-	else if (user == "user2")
-	{
-		initial = 1;
-	}
 	
 	switch (userValue)
 	{
-		case 1 :
-			array[0][0] =   initial;
+		case 'a' :
+			array[0][0] = user;
 			break;
-		case 2:
-			array[0][1] = initial;
+		case 'b' :
+			array[0][1] = user;
 			break;
-		case 3:
-			array[0][2] = initial;
+		case 'c':
+			array[0][2] = user;
 			break;
-		case 4:
-			array[1][0] = initial;
+		case 'd':
+			array[1][0] = user;
 			break;
-		case 5:
-			array[1][1] = initial;
+		case 'e':
+			array[1][1] = user;
 			break;
-		case 6:
-			array[1][2] = initial;
+		case 'f':
+			array[1][2] = user;
 			break;
-		case 7:
-			array[2][0] = initial;
+		case 'g':
+			array[2][0] = user;
 			break;
-		case 8:
-			array[2][1] = initial;
+		case 'h':
+			array[2][1] = user;
 			break;
-		case 9:
-			array[2][2] = initial;
+		case 'i':
+			array[2][2] = user;
 			break;
 		default:
 			printf("Please enter a valid input");
